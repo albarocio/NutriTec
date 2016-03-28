989d6ddf8e4ddf70bf1adaa82c1c5eb0f0c86ad7
Israel Calderón de la Cruz <icalderond@outlook.com>
2016-03-28 09:17:50 -0500

    Obtencion y envio de datos desde post en consulta

--- routes/consulta.js
+++ routes/consulta.js
@@ -1,8 +1,24 @@
 var express = require('express');
+var url = require('url');
+var math = require('mathjs')
+
+
 var router = express.Router();
 
 router.get('/salud', function(req, res, next) {
-  res.render('consulta', { title: 'Nutri-Tec' });
+	
+	var url_parts = url.parse(req.url, true);
+	var query = url_parts.query;
+
+	var idPeso=Number(query.idPeso)
+	var idAltura=Number(query.idAltura)
+	var result=math.divide(idPeso,(idAltura*idAltura))
+
+	idPeso=idPeso>0?idPeso:0;
+	idAltura=idAltura>0?idAltura:0;
+	result=result>0?result:0;
+
+  	res.render('consulta', { title: 'Nutri-Tec',masa:result, peso:idPeso, altura:idAltura});
 });
 
 router.get('/registro', function(req, res, next) {
@@ -12,5 +28,17 @@ router.get('/registro', function(req, res, next) {
 router.get('/sesion', function(req, res, next) {
   res.render('inicio-sesion', { title: 'Nutri-Tec' });
 });
+
+// router.get('/consultar', function(req, res, next) {
+// 	var url_parts = url.parse(req.url, true);
+// 	var query = url_parts.query;
+// 	var idPeso=Number(query.idPeso)
+// 	var idAltura=Number(query.idAltura)
+// 	var result=math.divide(idPeso,(idAltura*idAltura))
+// 	// console.log(idAltura)
+// 	// console.log(idPeso)
+// 	console.log(result)
+// 	res.render('consulta', { title: 'Nutri-Tec',masa:result, peso:idPeso, altura:idAltura});
+// });
 
 module.exports = router;
