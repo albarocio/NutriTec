14cc04cd2d63a0397d2ea6a7b0d04d927590b93a
Israel Calderón de la Cruz <icalderond@outlook.com>
2016-04-03 19:44:44 -0500

    Implementacion del metodo para registro

--- routes/user.js
+++ routes/user.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var url = require('url');
+var db=require('../db')
 
 var router = express.Router();
 
@@ -6,7 +8,25 @@ var router = express.Router();
 var TITLE='Nutri-Tec'
 
 router.get('/registro', function(req, res, next) {
-  res.render('registro', { title: TITLE });
+
+	var url_parts = url.parse(req.url, true);
+	var query = url_parts.query;
+
+	if(query.idNombre!='' && query.idCorreo!='' && query.idPassword!=''){
+		
+		var persona={
+			nombre:query.idNombre,
+			correo:query.idCorreo,
+			password:query.idPassword
+		}
+	  	var id=db.createPersona(persona)
+	  	res.cookie('persona',persona, { maxAge: 900000, httpOnly: true });
+	  }
+	if(!req.cookies.persona){
+  		res.render('registro', { title: TITLE });
+  	}else{
+  		 res.redirect('/consulta/salud');
+  	}
 });
 
 router.get('/sesion', function(req, res, next) {
