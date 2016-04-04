ed5aff314ad60c0b7684aa40197d004799d67a2d
Israel Calderón de la Cruz <icalderond@outlook.com>
2016-04-03 19:47:03 -0500

    Mejoras para mostrar los datos en la vista de consulta

--- routes/consulta.js
+++ routes/consulta.js
@@ -5,7 +5,6 @@ var math = require('mathjs')
 
 var router = express.Router();
 
-/*Constantes*/
 var TITLE='Nutri-Tec'
 
 router.get('/salud', function(req, res, next) {
@@ -13,15 +12,31 @@ router.get('/salud', function(req, res, next) {
 	var url_parts = url.parse(req.url, true);
 	var query = url_parts.query;
 
-	var idPeso=Number(query.idPeso)
-	var idAltura=Number(query.idAltura)
-	var result=math.divide(idPeso,(idAltura*idAltura))
+	if(isNumeric(query.idPeso) &&
+		isNumeric(query.idAltura)){
 
-	idPeso=idPeso>0?idPeso:0;
-	idAltura=idAltura>0?idAltura:0;
-	result=result>0?result:0;
+		var idPeso=Number(query.idPeso)
+		var idAltura=Number(query.idAltura)
+		var result=math.divide(idPeso,(idAltura*idAltura))
 
-  	res.render('consulta', { title: TITLE,masa:result, peso:idPeso, altura:idAltura});
+		if(res.cookies.persona){
+			// Insertar el registro a la persona indicada
+		}else{
+			// Insertar el registro a persona_detalle sin una relacion de persona
+		}
+  	
+  		res.render('consulta', 
+  			{ 
+  				title: TITLE,
+  				masa:result, 
+  				peso:idPeso, 
+  				altura:idAltura
+  			});
+	}
+  	res.render('consulta', { title: TITLE });
 });
+function isNumeric(n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
+}
 
 module.exports = router;
